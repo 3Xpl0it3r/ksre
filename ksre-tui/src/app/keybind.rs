3d0a3d52a26a5ecae68120a09d87bbb2e7c4e6eb
l0calh0st <shouc.wang@hotmail.com>
2024-03-01 11:26:43 +0800

    refactor(keybind): more convenice to config keybind

--- ksre-tui/src/app/keybind.rs
+++ ksre-tui/src/app/keybind.rs
@@ -19,44 +19,39 @@ macro_rules! keybind_struct {
 keybind_struct!(
     a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, tab, tick
 );
-
 macro_rules! generate_keybinding {
-    (( $( $ele:ident ),* )) => {
+    (@default_keybinding  $( $ele:ident ),* ) => {
         KeyBinding {
             $( $ele: None ),*
         }
     };
-    (( $( $key:ident : $value:ident ),* )) => {
-        KeyBinding {
-            $( $key: Some($value) ),*
-        }
+    (@empty) => {
+        generate_keybinding!(@default_keybinding a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, tab, tick)
     };
-    (( $( $ele:ident ),+ ) , ( $($key:ident : $value:path),+ )) => {
-        KeyBinding {
-            $( $ele: None ),* ,
-            $( $key: Some($value) ),*
+    ($( $key:ident : $value:path ),*) => {
+        {
+            let mut empty_keybindings = generate_keybinding!(@empty);
+            $(empty_keybindings.$key = Some($value));*;
+            empty_keybindings
         }
     };
 }
 
 pub const DEFAULT_POD_KEYBINDING: KeyBinding = generate_keybinding! {
-    (a,b,c,d,e,f,g,h,i,m,n,o,p,r,s,u,v,w,x,y,z,tab, tick),
-    // custom define keyactions
-    (j : App::select_items_next, k: App::select_items_prev, l:App::handle_pod_logs, q:App::handle_quit, t: App::handle_pod_exec)
+    j: App::select_items_next,
+    k: App::select_items_prev,
+    l: App::handle_pod_logs,
+    q: App::handle_quit,
+    t: App::handle_pod_exec
 
 };
 
 pub const DEFAULT_DEPLOYMENT_KEYBINDING: KeyBinding = generate_keybinding! {
-    (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,r,s,t,u,v,w,x,y,z,tab, tick),
-    // custom define keyactions
-    (q: App::handle_quit)
+    q: App::handle_quit
 };
 
 pub const DEFAULT_NODE_KEYBINDING: KeyBinding = generate_keybinding! {
-    (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,r,s,t,u,v,w,x,y,z,tab, tick),
-    (q: App::handle_quit)
+    q: App::handle_quit
 };
 
-pub const DEFAULT_NOOP_KEYBINDING: KeyBinding = generate_keybinding! {
-    (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, tab, tick)
-};
+pub const DEFAULT_NOOP_KEYBINDING: KeyBinding = generate_keybinding!(@empty);
